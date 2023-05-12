612dd63792e2e60c99a1f03ef3f4cd41fffd1fbb
Joseluito71 <110116614+Joseluito71@users.noreply.github.com>
2023-05-12 11:20:55 +0200

    nuevo fondo

--- dragonboatracing/core/src/com/barquitosfc/game/DragonBoatGame.java
+++ dragonboatracing/core/src/com/barquitosfc/game/DragonBoatGame.java
@@ -556,7 +556,7 @@ public class DragonBoatGame extends ApplicationAdapter {
 //					camfla.position.x = minijuego.jugador.getX();
 				
 			//	omega.play();
-	    	
+	    	 currentFrame= minijuego.animacion.getKeyFrame(deltaTime*1890000, true);
 	    	 		camfla.update();
 				    leftLimitmini = camfla.position.x - 1920 / 2;
 				    rightLimitmini = camfla.position.x + 1920 / 2;
@@ -568,7 +568,7 @@ public class DragonBoatGame extends ApplicationAdapter {
 				   
 				   
 				    if(Gdx.input.isKeyPressed(Keys.SPACE)&&minijuego.jugador.getY()<1450) {
-				    	currentFrame= minijuego.animacion.getKeyFrame(deltaTime*600000, true);
+				    	
 				    	minijuego.jugador.setY((float) (minijuego.jugador.getY()+5.2*deltaTime));
 				    }
 				    if (Gdx.input.isKeyPressed(Keys.ESCAPE)) {
